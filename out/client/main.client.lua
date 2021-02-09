94c8268036aa1d0890b22f8c0126a984cd5353c7
tacheometry <39647014+tacheometry@users.noreply.github.com>
2021-02-09 22:52:24 +0200

    Delete out directory

--- out/client/main.client.lua
+++
@@ -1,4 +0,0 @@
--- Compiled with roblox-ts v1.0.0-beta.15
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local makeHello = TS.import(script, game:GetService("ReplicatedStorage"), "TS", "module").makeHello
-print(makeHello("main.client.ts"))
