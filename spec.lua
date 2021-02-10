9aecc49aee2bfb910489b1f65689795844f4d28a
tacheometry <39647014+tacheometry@users.noreply.github.com>
2021-02-10 09:53:39 +0200

    Update spec.lua

--- spec.lua
+++ spec.lua
@@ -5,6 +5,8 @@ local TestEZ = require(ReplicatedStorage.TS.TestEZ)
 local testsFolder = ReplicatedStorage.TS.Tests
 local results = TestEZ.TestBootstrap:run({testsFolder}, TestEZ.Reporters.TextReporter)
 
+error("Does this run?")
+
 if #results.errors > 0 or results.failureCount > 0 then
 	error("Tests failed!")
 end
