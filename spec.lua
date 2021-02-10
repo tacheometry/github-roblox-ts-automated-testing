3d54ebee45845968a90a5d0efeaaf67b8a78bf72
tacheometry <39647014+tacheometry@users.noreply.github.com>
2021-02-10 10:08:15 +0200

    Update spec.lua

--- spec.lua
+++ spec.lua
@@ -3,10 +3,4 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TestEZ = require(ReplicatedStorage.TS.TestEZ)
 
 local testsFolder = ReplicatedStorage.TS.Tests
-local results = TestEZ.TestBootstrap:run({testsFolder}, TestEZ.Reporters.TextReporter)
-
-error("Does this run?")
-
-if #results.errors > 0 or results.failureCount > 0 then
-	error("Tests failed!")
-end
+TestEZ.TestBootstrap:run({testsFolder}, TestEZ.Reporters.TextReporter)
